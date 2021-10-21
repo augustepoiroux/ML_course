9cf1008349630009a714a6ff3902c93de8168703
augustepoiroux <augustepoiroux@gmail.com>
2021-10-21 17:43:14 +0200

    TD5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -9,9 +9,7 @@ import numpy as np
 
 def least_squares(y, tx):
     """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    w = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    e = y - tx @ w
+    mse = np.squeeze(e.T @ e) / (2 * len(y))
+    return mse, w
